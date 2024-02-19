840c6111d89992d3e7cb7fafc353e19e4d62f267
Shuvo <11577894+shuvok8@users.noreply.github.com>
2024-02-19 11:08:31 +0600

    Name Export method 2

--- createmodule/calculator.js
+++ createmodule/calculator.js
@@ -1,7 +1,8 @@
-let sumData=(a,b) => {
+export let sumData=(a,b) => {
     return a+b;
 }
-let minData=(a,b) => {
+export let minData=(a,b) => {
     return a-b;
 }
-export {sumData,minData}
+export let cName="Hello World"
+// export {sumData,minData}
--- createmodule/index.js
+++ createmodule/index.js
@@ -1,4 +1,5 @@
-import {sumData, minData} from "./calculator.js";
+import {sumData, minData, cName} from "./calculator.js";
 
 console.log (sumData(10,20))
 console.log (minData(10,20))
+console.log (cName)
